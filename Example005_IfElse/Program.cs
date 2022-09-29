a634386cc2c3385dcebe1985a1b1200043819c8b
Kiriakov Aleksandr <san4ousoff@mail.ru>
2022-09-29 10:41:50 +0200

    add Example 005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,17 @@
+﻿// Особое приветствие определенного пользователя по имени
+//string username = "Маша";
+//Console.WriteLine(username);
+Console.Write("Введите Ваше имя: ");
+//string anothername = Console.ReadLine();
+string username = Console.ReadLine();
+//if (username == anothername)
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Божечки мои, да это же Маша!");
+}
+else
+{
+    Console.Write("А, это опять ты, ");
+//    Console.WriteLine(anothername);
+    Console.WriteLine(username);
+}
