d389ee896754e8f52e88cee49a74a5b3e336b3aa
Kiriakov Aleksandr <san4ousoff@mail.ru>
2022-10-02 23:09:42 +0200

    add Example 010 FindIndex

---
+++ Example010_FindIndex/Program.cs
@@ -0,0 +1,19 @@
+﻿// Вывод первого индекса искомого числа в массиве
+Console.Clear();
+
+int[] array = {21, 43, 65, 43, 45, 34};
+int length = array.Length;
+int count = 0;
+
+Console.Write("Введите искомое число: ");
+int find = int.Parse(Console.ReadLine());
+
+while (count < length)
+{ 
+    if (array[count] == find) 
+    {
+        Console.WriteLine(count);
+        break;
+    }    
+count++;
+}
